99412783c5670aa50ab6ed09a734eae94f9dbaaa
kh-saadany <kh.saadany@gmail.com>
2026-05-28 11:46:10 +0300

    Update service-worker.js to Version 2 Release 8 (No scroll home screen)

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "rakah-counter-v22";
+const CACHE_NAME = "rakah-counter-v23";
 const ASSETS_TO_CACHE = [
   "./",
   "index.html",
-  "style.css?v=12",
-  "app.js?v=12",
+  "style.css?v=13",
+  "app.js?v=13",
   "manifest.json"
 ];
 
