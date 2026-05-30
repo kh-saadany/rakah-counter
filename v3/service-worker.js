ec97349bf4164e3c2b57e543f45155666255c84c
kh-saadany <kh.saadany@gmail.com>
2026-05-30 06:12:03 +0300

    Deploy Rakah Counter v3 (Absolute threshold only) - service-worker.js

--- v3/service-worker.js
+++ v3/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "rakah-counter-v31";
+const CACHE_NAME = "rakah-counter-v32";
 const ASSETS_TO_CACHE = [
   "./",
   "index.html",
-  "style.css?v=31",
-  "app.js?v=31",
+  "style.css?v=32",
+  "app.js?v=32",
   "manifest.json"
 ];
 
